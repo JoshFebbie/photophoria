776501c4d43164d68937b86d2b792c054432a6c1
josh <joshdfebbie@gmial.com>
2024-03-24 12:34:16 -0700

    Form_Sub

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,15 +1,21 @@
 import Header from '@/components/shared/Header'
 import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
 
 const AddTransformationTypePage = ({ params: { type } }:
   SearchParamProps) => {
     const transformation = transformationTypes[type]
 
   return (
+    <>
     <Header
       title={transformation.title}
       subtitle={transformation.subTitle}
     />
+
+    <TransformationForm />
+
+    </>
   )
 }
 
