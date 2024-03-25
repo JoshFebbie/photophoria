23d6114cd0c441ce1b77c749104f728926944eda
josh <joshdfebbie@gmial.com>
2024-03-25 03:27:27 -0700

    bad

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,20 +1,72 @@
-import Header from '@/components/shared/Header'
-import { transformationTypes } from '@/constants'
-import TransformationForm from '@/components/shared/TransformationForm'
+// import Header from "@/components/shared/Header";
+// import TransformationForm from "@/components/shared/TransformationForm";
+// import { transformationTypes } from "@/constants";
+// import { getUserById } from "@/lib/actions/user.actions";
+// import { auth } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }:
-  SearchParamProps) => {
-    const transformation = transformationTypes[type]
+// const AddTransformationTypePage = async ({
+//   params: { type },
+// }: SearchParamProps) => {
+//   const { userId } = auth();
+//   const transformation = transformationTypes[type];
+
+//   if (!userId) redirect("/sign-in");
+
+//   const user = await getUserById(userId);
+
+//   return (
+//     <>
+//       <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+//       <section className="mt-10">
+//         <TransformationForm
+//           action="Add"
+//           userId={user._id}
+//           type={transformation.type as TransformationTypeKey}
+//           creditBalance={user.creditBalance}
+//         />
+//       </section>
+//     </>
+//   );
+// };
+
+// export default AddTransformationTypePage;
+
+
+
+// Replacement for the above snippet //
+
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
 
   return (
     <>
-    <Header
-      title={transformation.title}
-      subtitle={transformation.subTitle}
-    />
-
-    <TransformationForm />
-
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   )
 }
